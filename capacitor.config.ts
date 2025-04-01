043cc43b521ca7f8b7dc8bf7efbc67a6af7afd2b
MarianaC9910 <21152@portalcedi.edu.mx>
2025-04-01 09:48:00 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 's13_eas22',
+  webDir: 'www'
+};
+
+export default config;
